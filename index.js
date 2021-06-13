38da890eec20c817fccaea62f2542fd76809a882
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-06-13 01:00:36 -0400

    adds fetch function

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ document.addEventListener('DOMContentLoaded', () => {
 function getBook() {
     fetch(endPoint)
     .then(res => res.json())
-    .then(json => console.log(json));
+    .then(json => { 
+        json.data.forEach(book => {
+            const bookMarkup = `
+          <div data-id=${book.id}>
+            <img src=${book.attributes.book_img} height="200" width="250">
+            <h3>${book.attributes.title}</h3>
+            <p>${book.attributes.genre.name}</p>
+            <button data-id=${book.id}>edit</button>
+          </div>
+          <br><br>`;
+
+          document.querySelector('#book-container').innerHTML += bookMarkup
+        })
+
+    });
 }
 
