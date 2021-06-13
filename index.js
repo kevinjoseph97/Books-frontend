ed054102fdfd9f91e91a0d0dc5f6b5fa00dd0a04
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-06-13 00:31:44 -0400

    adds intial fetch request

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("HOOOOO ")
+const endPoint = "http://localhost:3000/api/v1/books"
+
+
+// console log once the DOM is loaded 
+document.addEventListener('DOMContentLoaded', () => {
+    getBook()
+  
+});
+
+function getBook() {
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
