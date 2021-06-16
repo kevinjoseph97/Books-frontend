92d7ca713f74db45f5fa0a51e903427b4f2ecaf4
kevinjoseph97 <kevinjoseph97@gmail.com>
2021-06-16 13:16:06 -0400

    debugged post

--- src/book.js
+++ src/book.js
@@ -8,19 +8,22 @@ class Book {
         this.title =  bookAttributes.title 
         this.author =  bookAttributes.author
         this.book_img =  bookAttributes.book_img
-        this.genre =  bookAttributes.genre
+        this.genre =  bookAttributes.genre.name
         Book.all.push(this)
     
     } 
-    render() {
-        debugger
+    renderBook() {
+        // debugger
         // console.log(this);
         return `
                 <div data-id=${this.id}>
                   <img src=${this.book_img} height="200" width="250">
                   <h3>${this.title}</h3>
-                  <p>${this.name}</p>
+                  <p>${this.author}</p>
+                  <p>${this.genre}</p>
                   <button data-id=${this.id}>edit</button>
+                  <button data-id="${this.id}" class="delete-btn">Delete</button>
+                  
                 </div>
                 <br><br>`;
       
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const endPoint = "http://localhost:3000/api/v1/books"
+// let divBook = document.querySelector('#book-container')
 
-
+document.addEventListener("click", (e) =>{ console.log("You just clicked on", e.target)})
 // console log once the DOM is loaded 
 document.addEventListener('DOMContentLoaded', () => {
     getBook()
     
     // get the data from the form 
     const createBookForm = document.querySelector("#create-book-form")
-
     // event to listen for the click on the form and prvent the default 
     createBookForm.addEventListener("submit", (e) => createFormHandler(e))
   
@@ -20,7 +20,7 @@ function getBook() {
         book.data.forEach(book => {
           // debugger;
           let newBook = new Book(book, book.attributes )
-          document.querySelector('#book-container').innerHTML += newBook.render()
+          document.querySelector('#book-container').innerHTML += newBook.renderBook()
           
         })
 
@@ -31,7 +31,7 @@ function getBook() {
 
 // implement a form handler adn make POST request
 function createFormHandler(e) {
-  e.preventDefault()
+  e.preventDefault();
   // grab all the values
   // debugger;
   const userTitle = document.querySelector("#input-title").value
@@ -40,12 +40,39 @@ function createFormHandler(e) {
   const userGenre = document.querySelector("#genres").value
   const genreId = parseInt(userGenre)
   postFetch(userTitle, userAuthor, userImg, genreId)
+
+  function postFetch(title, author, book_img, genre_id) {
+    // console.log(title, author, book_img, genre_id);
+    fetch(endPoint, {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        title: title,
+        author: author,
+        book_img: book_img,
+        genre_id: genre_id
+      })
+    })
+    .then(response => response.json())
+    .then(book => {
+      // console.log(book);
+      const bookData = book.data
+      let newBook = new Book(bookData, bookData.attributes )
+      document.querySelector('#book-container').innerHTML += newBook.renderBook()
+  
+  
+    }) 
+  
+    
+      
+  }
+  e.target.reset()
 }
 
 
 function postFetch(title, author, book_img, genre_id) {
   // console.log(title, author, book_img, genre_id);
-  fetch("http://localhost:3000/api/v1/books", {
+  fetch(endPoint, {
     method: 'POST',
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
@@ -59,18 +86,39 @@ function postFetch(title, author, book_img, genre_id) {
   .then(book => {
     // console.log(book);
     const bookData = book.data
-    const bookMarkup = `
-    <div data-id=${book.id}>
-      <img src=${bookData.attributes.book_img} height="200" width="250">
-      <h3>${bookData.attributes.title}</h3>
-      <p>${bookData.attributes.genre.name}</p>
-      <button data-id=${book.id}>edit</button>
-    </div>
-    <br><br>`;
+    let newBook = new Book(bookData, bookData.attributes )
+    document.querySelector('#book-container').innerHTML += newBook.renderBook()
 
-    document.querySelector('#book-container').innerHTML += bookMarkup
-  })
+
+  }) 
+
+  e.target.reset()
     
-  }
+}
+
+
+
+
+// delete book function
+const selectedBook = document.querySelector("#book-container")
+selectedBook.addEventListener("click", event =>{event.preventDefault();
+    if (event.target.matches(".delete-btn")){
+      console.log(event.target)
+
+      const id = event.target.dataset.id
+        const deleteBook = document.getElementById(id)
+      
+    
+      fetch(`${endPoint}/${id}`, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json"}
+      })
+      .then(response => response.json())
+      .then(deleteBook.remove())
+
+    
+    }
+})
+
 
 
